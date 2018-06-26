af670fa78c8344eb24892ba726836ab816a89b74
ankitbhardwaj78 <bhardwajankit438@gmail.com>
2018-06-26 14:06:21 +0530

    Hero list added

---
+++ src/app/heroes-list.ts
@@ -0,0 +1,10 @@
+import { Hero } from "./hero";
+
+export const Heroes: Hero[] = [
+    { id: 1, name: "messi" },
+    { id: 2, name: "leonardo" },
+    { id: 3, name: "Bryan Cranston" },
+    { id: 4, name: "Kit" },
+    { id: 5, name: "RObert" }
+
+]
